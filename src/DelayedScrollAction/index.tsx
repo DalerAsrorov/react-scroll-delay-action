036d05189ddc509f193a043450cd0dc0970d586a
Daler Asrorov <asrorids@gmail.com>
2018-11-17 09:18:35 -0800

    Added more tests

--- src/DelayedScrollAction/index.tsx
+++ src/DelayedScrollAction/index.tsx
@@ -1,36 +1,29 @@
 import React, { PureComponent } from 'react';
 import { getParent, isScrolledIntoView } from './utils';
 
-interface Props {
-  id: any;
-  delayTime: number;
-  onEnd: (id: any) => void;
-  onStart?: (id: any) => void;
-  callOnce?: boolean;
-  parentNode?: any | Window;
-}
-
-interface State {
-  isVisible: boolean;
-}
-
-export default class DelayedScrollAction extends PureComponent<Props, {}> {
+export default class DelayedScrollAction extends PureComponent<
+  ReactDelayedScrollProps,
+  ReactDelayedScrollState
+> {
   private nodeRef: any;
   private delayTimeout: any;
 
-  state = {
-    isVisible: false
-  };
-
-  constructor(props: Props) {
+  constructor(props: ReactDelayedScrollProps) {
     super(props);
 
     this.nodeRef = React.createRef();
+    this.state = {
+      parentNode: props.parentNode
+    };
   }
 
   componentDidMount() {
     const { id, delayTime, callOnce, parentNode, onStart, onEnd } = this.props;
     const parent = getParent(parentNode);
+
+    this.setState({
+      parentNode: getParent(parent)
+    });
 
     if (onEnd) {
       parent.addEventListener('scroll', () => {

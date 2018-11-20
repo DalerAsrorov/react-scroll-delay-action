dad63999b91162863a2cd6bb22aef807be91986d
Daler Asrorov <asrorids@gmail.com>
2018-11-20 03:22:18 -0800

    Publish progress so far

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './demos/src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/demos/src/'
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+    ]
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'demos/src'),
+    compress: true,
+    port: 3000
+  }
+};
